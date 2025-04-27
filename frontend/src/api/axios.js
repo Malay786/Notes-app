d13fc30607379b890a6e339ff7f8959622d26beb
Malay Dewari <dewarimalay@gmail.com>
2025-04-27 10:39:58 +0530

    Update frontend with latest code changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api",
+    // baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_BACKEND_URL,  // use vercel environmen variable
+    withCredentials: true,  // so that cookies can be sent
 });
 
 // automatically attach token
