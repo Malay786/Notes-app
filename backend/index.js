6d695ea31e3f6fb8013c0e6e12bfcdb9c267dc41
Malay Dewari <dewarimalay@gmail.com>
2025-04-27 11:16:30 +0530

    removed / from the end of vercel link

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,13 @@ import noteRoutes from './routes/noteRoutes.js';
 
 const app = express();
 
+// const frontendUrl = 'notes-app-git-main-dewarimalay-gmailcoms-projects.vercel.app'
 app.use(cors({
-    origin: "https://notes-app-git-main-dewarimalay-gmailcoms-projects.vercel.app/",  // Vercel frontend URL
-    credentials: true
+    origin: "https://notes-app-git-main-dewarimalay-gmailcoms-projects.vercel.app",  // Vercel frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true   // Allow cookies (if needed)
 }));
+
 app.use(express.json());
 app.use("/api/auth", authRoutes)
 app.use("/api/notes", noteRoutes)
